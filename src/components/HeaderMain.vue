d3b930ed71962fa2b8a6705752e8bb03458633a7
Todd Nichols <nivram80@gmail.com>
2020-07-28 14:57:46 -0500

    add app scaffolding and roomlio component shell

---
+++ src/components/HeaderMain.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="main-header">
+    <h1>Francais</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderMain",
+};
+</script>
+
+<style scoped>
+.main-header {
+  display: flex;
+  align-items: center;
+  height: 40px;
+  padding: 8px 16px;
+  border-bottom: 1px solid lightgray;
+}
+
+h1 {
+  text-transform: uppercase;
+  color: darkred;
+}
+</style>
---
+++ src/components/Roomlio.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="roomlio-container">
+    <!-- <div data-rml-room data-rml-version="09.mar.2020" data-rml-pk="bnl8ln7budgo8g1bs1vg" /> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Roomlio",
+};
+</script>
+
+<style scoped>
+.roomlio-container {
+  width: 100%;
+  height: 600px;
+  border: 1px solid white;
+}
+</style>
